0138a6c23d302a460da224d06fbda158259b22a7
Lumic <3474889077@qq.com>
2026-03-18 12:03:59 +0800

    Update 8_codes.c

--- codes/8_codes.c
+++ codes/8_codes.c
@@ -7,3 +7,27 @@ int main(void)
     for(int i=0;i<10;i++) b[i] = a [i];
 	return 0;
 }
+
+//用rand函数生成[10, 20]之间的随机整数，表达式应该怎么写？
+#include <stdio.h>
+#include <stdlib.h>
+int rand_10_to_20(void)
+{   
+    int a;
+    while(1) 
+    {
+        a = rand();
+        if((a>=10)&&(a<=20))
+        {
+            return a;
+        }
+    }
+}
+int main(void)
+{
+    for(int i=0;i<10;i++) 
+    {
+        printf("Rand:%d\n",rand_10_to_20());
+    }
+	return 0;
+}
